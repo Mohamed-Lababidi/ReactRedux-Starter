f5e54a7564aac3e1334e957e6f689aef895b5407
Mohamed-Lababidi <lababidi.mohamed75@gmail.com>
2020-08-14 03:09:37 +0200

    props and map terminated

---
+++ src/components/search-bar.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+ constructor(props){
+     super(props);
+     this.state = {searchText:"",placeholder:"Tapez votre film.."}
+ }
+  render() {
+    return ( 
+        <div>
+            <input onChange={this.handleChange.bind(this)} placeholder={this.state.placeholder}/>
+        </div>
+    )
+}
+    handleChange (event) {
+        this.setState({searchText:event.target.value});
+    }
+}
+export default SearchBar;
