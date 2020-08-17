d6ac0edc987f95c244a41cb44863e78c3cc40ce7
Mohamed-Lababidi <lababidi.mohamed75@gmail.com>
2020-08-17 03:07:53 +0200

    display video and 5 first movies

--- src/containers/app.js
+++ src/containers/app.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import SearchBar from "../components/search-bar";
 import VideoList from "./video-list";
 import VideoDetail from "../components/video-detail"
+import Video from "../components/video"
 import axios from "axios";
 
 const API_END_POINT = "https://api.themoviedb.org/3/"
@@ -14,17 +15,43 @@ class App extends Component {
     super(props);
     this.state = {movieList:{},currentMovie:{}};
   }
+  
   componentWillMount() {
-      axios.get(`${API_END_POINT}${POPULAR_MOVIES_URL}&${API_KEY}`).then(function(response){
-          this.setState({movieList:response.data.results.slice(1,6)})
-          this.setState({currentMovie:response.data.results[0]})
-      }.bind(this));
+      this.initMovies();
   }
+
+  initMovies(){
+
+        axios.get(`${API_END_POINT}${POPULAR_MOVIES_URL}&${API_KEY}`).then(function(response){
+          this.setState({movieList:response.data.results.slice(1,6), currentMovie:response.data.results[0]}, function(){
+          this.applyVideoToCurrentMovie();
+          })
+        }.bind(this));
+
+  }
+
+  applyVideoToCurrentMovie () {
+    axios.get(`${API_END_POINT}movie/${this.state.currentMovie.id}?${API_KEY}&append_to_response=videos&include_adult=false`).then(function(response){
+        console.log('', response)
+        const youtubeKey = response.data.videos.results[0].key;
+        let newCurrentMovieState = this.state.currentMovie;
+        newCurrentMovieState.videoId = youtubeKey;
+        this.setState({currentMovie : newCurrentMovieState});
+        console.log('',newCurrentMovieState)
+    }.bind(this));
+  }
+
+
   render() {
+      const renderVideoList = () => {
+          if(this.state.movieList.length>=5)
+          return <VideoList movieList={this.state.movieList}/>
+      }
     return (
       <div>
         <SearchBar />
-        <VideoList />
+        <Video videoId={this.state.currentMovie.videoId}/>
+        {renderVideoList()}
         <VideoDetail title={this.state.currentMovie.title} description={this.state.currentMovie.overview}/> 
       </div>
     );
