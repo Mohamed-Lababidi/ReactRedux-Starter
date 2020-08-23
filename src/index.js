b6f42d1b8531d30c879431cd56a514f8476eb6fc
Mohamed-Lababidi <lababidi.mohamed75@gmail.com>
2020-08-24 00:39:37 +0200

    project push in heroku finish

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './containers/app'
 
-// const API_KEY ="15c997966d7f306fb907fb8332fd350e";
+
 
 ReactDOM.render(<App/>,document.querySelector('.container'));
